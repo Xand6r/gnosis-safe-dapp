a43efb52dbc5a14e4a26f260aeb61a699c4598df
xander <shuaibualexander@gmail.com>
2022-09-03 22:40:10 +0100

    first commit

--- utils/connectors.ts
+++
@@ -1,9 +0,0 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
-
-const SUPPORTED_CHAIN_IDS = [
-  4, //for rinkeby
-];
-
-export const injected = new InjectedConnector({
-  supportedChainIds: SUPPORTED_CHAIN_IDS,
-});
